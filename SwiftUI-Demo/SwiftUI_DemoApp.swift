98ec8a32f028f8e915d29d1d6babcc840d818930
yangbiwan <yangbiwan@lasoplanet.com>
2024-11-14 10:20:41 +0800

    Initial Commit

---
+++ SwiftUI-Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI-Demo
+//
+//  Created by ybwdaisy on 2024/11/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-DemoTests/SwiftUI_DemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI_DemoTests.swift
+//  SwiftUI-DemoTests
+//
+//  Created by ybwdaisy on 2024/11/14.
+//
+
+import Testing
+
+struct SwiftUI_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
