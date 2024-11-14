c29c08e9ffe15f3966a1e17dea8af26546a3d0e8
yangbiwan <yangbiwan@lasoplanet.com>
2024-11-14 11:41:51 +0800

    TimelineView refresh demo: https://fatbobman.com/zh/posts/understanding-swiftui-view-update-mechanism/

--- SwiftUI-Demo/ContentView.swift
+++ SwiftUI-Demo/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        let _ = print("Evaluating View Declaration Value")
+        Text("Count: \(count)")
+        Text("Tap Me")
+            .onTapGesture {
+                count += 1
+            }
+        SubView1()
+        SubView2(count: count)
+    }
+}
+
+struct SubView1: View {
+    init() {
+        print("subview1 init")
+    }
+
+    var body: some View {
+        let _ = print("subview1 body update")
+        Text("No changes")
+    }
+}
+
+struct SubView2: View {
+    let count: Int
+    init(count: Int) {
+        self.count = count
+        print("subview2 init")
+    }
+
+    var body: some View {
+        let _ = print("subview2 body update")
+        Text("Count Changes: \(count)")
     }
 }
 
 #Preview {
-    ContentView()
+//    ContentView()
+    EmojiDemo()
 }
---
+++ SwiftUI-Demo/EmojiDemo.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiDemo.swift
+//  SwiftUI-Demo
+//
+//  Created by ybwdaisy on 2024/11/14.
+//
+import SwiftUI
+
+let emojis = ["😀", "😬", "😄", "🙂", "😗", "🤓", "😏", "😕", "😟", "😎", "😜", "😍", "🤪"]
+
+struct EmojiDemo: View {
+    var body: some View {
+        TimelineView(.periodic(from: .now, by: 0.2)) { timeline in
+            HStack(spacing: 120) {
+                let randomEmoji = emojis.randomElement() ?? ""
+                Text(randomEmoji)
+                    .font(.largeTitle)
+                    .scaleEffect(4.0)
+
+                RightEmoji()
+            }
+        }
+    }
+}
+
+struct RightEmoji: View {
+     let id: Int = .random(in: 0 ... 100_000) // 打开这个就可以更新
+    var body: some View {
+        let randomEmoji = emojis.randomElement() ?? ""
+
+        Text(randomEmoji)
+            .font(.largeTitle)
+            .scaleEffect(4.0)
+    }
+}
